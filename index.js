9241601f8f46df81ec1176dcf6b8c28e2838db03
csm-18 <112937168+csm-18@users.noreply.github.com>
2026-02-15 15:17:08 +0530

    buttons logic

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("working!")
+let input = document.getElementById("input")
+function appendNumber(num) {
+    input.value = input.value + num
+}
+
+function backspace() {
+    input.value = input.value.slice(0,-1)
+}
